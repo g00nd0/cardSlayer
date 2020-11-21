ab70d39225f86f4c60091e6a753c26ab14fbc777
g00nd0 <g00nd0@MitchT495s.localdomain>
2020-11-21 17:36:11 +0800

    Add function in JS to shuffle cards

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 const cardGrid = ["a","b","b","a"];
 
+const cardsAll = ["a","a","b","b","c","c","d","d",] // temp cards
+
+const cardArrayShuffle = (arr) => {
+    let index, newIndex, tempCard;
+    for (let i = 0; i < arr.length; i++) {
+        index = Math.floor(Math.random() * arr.length);
+        tempCard = arr.splice(index, 1);
+        newIndex = Math.floor(Math.random() * arr.length)
+        arr.splice(newIndex, 0, tempCard[0]);
+    }
+    return arr;
+}
+
+//const cardsInGame = 
+
 const gameLevelData = [{
-                        "level": 0,
-                        "numOfCards": 4,
-                        "demon": "Generic Zombie"
+                            "level": 0,
+                            "numOfCards": 4,
+                            "demon": "Zombie Soldier",
+                            "imgPath": ''
                         },
                         {
-                        "level": 1,
-                        "numOfCards": 8,
-                        "demon": "Zombie Soldier"
-                        }]
+                            "level": 1,
+                            "numOfCards": 6,
+                            "demon": "Zombie Sergeant",
+                            "imgPath": ''
+                        },
+                        {
+                            "level": 2,
+                            "numOfCards": 8,
+                            "demon": "Zombie Sergeant",
+                            "imgPath": ''
+                            }];
 
 let gameOverStatus = false;
 
@@ -22,3 +45,23 @@ const userChoice = (x,y) => {
 }
 
 userChoice(0,3)
+
+console.log(cardArrayShuffle(cardsAll));
+
+// $(() => {
+//     // run function here
+// });
+
+// fn for: 
+// cardArrayShuffle() // shuffles positions of the cards
+// cardsInGame() //cards arranged in array
+// cardsChosen() // 2 element array for 2 cards chosen, clear cards after match/no match
+// cardsPaired() // cards won/paired are stored here
+// flipCard() // flip card to front or back when user chooses card, or when both cards revealed
+
+
+
+
+
+
+
