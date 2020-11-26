0c417003bd4201dec0d6cfb94a49f2e6791a5e56
g00nd0 <g00nd0@MitchT495s.localdomain>
2020-11-26 09:04:27 +0800

    Add powerup properties for cards, refactor parts of code

--- js/app.js
+++ js/app.js
@@ -1,140 +1,197 @@
 const cardsAll = [
                     {
                         id: "card0",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card1",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card2",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card3",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card4",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card5",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card6",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card7",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card8",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card9",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card10",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card11",
+                        type: "powerUp",
+                        power: "Shotgun",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card12",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card13",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card14",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card15",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card16",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card17",
+                        type: "powerUp",
+                        power: "Clairvoyance",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card18",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card19",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card20",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card21",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card22",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card23",
+                        type: "powerUp",
+                        power: "R.P.G. (Ridiculously overPowered Gun)",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card24",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card25",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card26",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card27",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/iconOfSin.png"
                     },
                     {
                         id: "card28",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/cannotDie.gif"
                     },
                     {
                         id: "card29",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/soulsphere.gif"
                     },
                     {
                         id: "card30",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     },
                     {
                         id: "card31",
+                        type: "normal",
+                        power: "none",
                         imgPath: "img/invisible.gif"
                     }
 ]
-
-// create object for cards instead
-// use following attributes:
-// id: (id of the card for html)
-// name: (text name for diplaying card)
-// imgPath: (path of image)
-// specialFx: (for card power ups later in game)
 
 const gameLevelData = [
                         {
@@ -175,7 +232,7 @@ const gameLevelData = [
                         },
                         {
                             "level": 6,
-                            "numOfCards": 25,
+                            "numOfCards": 24,
                             "demon": "Cerberus",
                             "imgPath": ''
                         },
@@ -384,17 +441,11 @@ const lessPlayerHealth = () => {
 const setEventFlipCard = () => {
     $(".display").children().on("click", () => {
         flipCard(event.target.id);
-        // console.log(event.target.id)
-        // if (gameStatus.gameOverStatus === true){
-        //     console.log("you lose")
-        // }
-        
     });
 }
 
 const removeEventFlipCard = (cardIdOnGrid) => {
     $(".display").children(cardIdOnGrid).off("click");
-
 }
 
 const startGameButton = () => {
